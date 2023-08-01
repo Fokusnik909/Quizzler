52075d741f861920b9f207e8f74e435320aee8d2
Arthur <Fokusnik909@mail.ru>
2023-08-01 14:32:38 +0500

    Initial Commit

---
+++ Quizzler/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quizzler
+//
+//  Created by Артур  Арсланов on 01.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
